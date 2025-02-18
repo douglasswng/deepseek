aeb4763a33084a4a367cf4fa1333eb91663095c8
douglasswng <douglasswng@gmail.com>
2025-02-18 09:30:15 +0800

    changed hyperparameters

--- deepseek-v3/model/args.py
+++ deepseek-v3/model/args.py
@@ -3,11 +3,11 @@ from dataclasses import dataclass
 @dataclass()
 class ModelArgs:
     vocab_size: int = 10000
-    num_layers: int = 12
+    num_layers: int = 16
 
     hidden_dim: int = 512
     latent_dim: int = 128
-    num_heads: int = 4
+    num_heads: int = 8
     moe_inter_dim: int = 128
 
     rope_dim: int = 64
@@ -15,6 +15,6 @@ class ModelArgs:
     max_seq_len: int = 1024
 
     num_shared_experts: int = 2
-    num_routed_experts: int = 32
-    num_active_experts: int = 6
+    num_routed_experts: int = 64
+    num_active_experts: int = 8
     bias_update_speed: float = 0.01
