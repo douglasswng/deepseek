dffe059ed69a878540ac30c353ef90911cacf5e3
douglasswng <douglasswng@gmail.com>
2025-02-20 14:17:16 +0800

    changed model args

--- deepseek-v3/model/args.py
+++ deepseek-v3/model/args.py
@@ -3,18 +3,18 @@ from dataclasses import dataclass
 @dataclass()
 class ModelArgs:
     vocab_size: int = 50000
-    num_layers: int = 16
+    num_layers: int = 19
 
-    hidden_dim: int = 512
-    latent_dim: int = 128
+    hidden_dim: int = 1024
+    latent_dim: int = 256
     num_heads: int = 8
-    moe_inter_dim: int = 128
+    moe_inter_dim: int = 512
 
     rope_dim: int = 64
     rope_theta: int = 10000
     max_seq_len: int = 1024
 
     num_shared_experts: int = 2
-    num_routed_experts: int = 64
-    num_active_experts: int = 8
+    num_routed_experts: int = 42
+    num_active_experts: int = 4
     bias_update_speed: float = 0.01
